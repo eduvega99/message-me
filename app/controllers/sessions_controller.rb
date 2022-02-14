589d406f3a7874f46fee63c495265bd5e5f83a71
“Eduardo <eduardovr1999@gmail.com>
2022-02-14 10:09:01 +0000

    Add authentications method on UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
   
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:sessions][:username])
+    if user && user.authenticate(params[:sessions][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in succesfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out"
+    redirect_to login_path
+  end
+
 end
